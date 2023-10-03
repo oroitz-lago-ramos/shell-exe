3f846aca2e904152e9f06e75e9613ecb99c587ab
oroitz-lago-ramos <oroitz.lago-ramos@laplateforme.io>
2023-10-03 11:34:21 +0200

    Final Job08

--- Job08/get_logs.sh
+++ Job08/get_logs.sh
@@ -1,10 +1,13 @@
-
+#!/bin/bash
 
 date_actuelle=$(date +"%d-%m-%Y-%H:%M")
-echo "$date_actuelle"
 # sudo cat /var/log/auth.log | grep "session opened" > number_connection-$date_actuelle
+
 logfile="/var/log/auth.log"
 connexions=$(sudo grep "session open" "$logfile")
 nombre_connexions=$(echo "$connexions" | wc -l)
+nom_fichier="number_connection-$date_actuelle"
 
-echo "$nombre_connexions" > number_connection-$date_actuelle
+echo "$nombre_connexions" > "$nom_fichier"
+tar -cvf Backup/$nom_fichier.tar $nom_fichier
+sudo rm $nom_fichier
