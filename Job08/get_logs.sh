4189e46b2b565e5b37f0be9135647f14d6fe1183
oroitz-lago-ramos <oroitz.lago-ramos@laplateforme.io>
2023-10-04 23:23:28 +0200

    Cleaning job 08

--- Job08/get_logs.sh
+++ Job08/get_logs.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 date_actuelle=$(date +"%d-%m-%Y-%H:%M")
-# sudo cat /var/log/auth.log | grep "session opened" > number_connection-$date_actuelle
-
 logfile="/var/log/auth.log"
 connexions=$(sudo grep "session open" "$logfile")
 nombre_connexions=$(echo "$connexions" | wc -l)
+
 fin_fichier="number_connection-$date_actuelle"
 nom_fichier="/home/oroitz/Documents/shell-exe/Job08/number_connection-$date_actuelle"
 
@@ -13,4 +12,3 @@ echo "$nombre_connexions" > "$nom_fichier"
 tar -cvf /home/oroitz/Documents/shell-exe/Job08/Backup/$fin_fichier.tar $nom_fichier
 
 sudo rm $nom_fichier
-echo "Script recupération du nombre de logs exécuté"
