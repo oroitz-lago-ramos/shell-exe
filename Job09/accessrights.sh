06b124cc84ce24e1f14647002d6d3859d11e584d
oroitz-lago-ramos <oroitz.lago-ramos@laplateforme.io>
2023-10-04 14:19:40 +0200

    commit test

--- Job09/accessrights.sh
+++ Job09/accessrights.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+while IFS=, read -r id prenom nom mdp role; do
+id=${id:0:(${#id}-1)}
+prenom=${prenom:0:(${#prenom}-1)}
+nom=${nom:0:(${#nom}-1)}
+mdp=${mdp:0:(${#mdp}-1)}
+role=${role:0:(${#role}-1)}
 
-while IFS=, read -r field1 field2 field3 field4 field5
-do
-    echo "$field1, $field2, $field3, $field4, $field5"
-    sudo useradd "$field1"
-    sudo passwd "$field4"
+
+echo "$role"
 done < Shell_Userlist.csv
